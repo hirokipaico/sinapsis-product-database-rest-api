69edf39970528a7f24a1c92f8075334193afa4ba
Hiroki Paico <hiroki.paico@hotmail.com>
2023-07-31 23:11:54 -0500

    chore: Remove Logger

--- src/common/pipes/xss-validation/xss-validation.pipe.ts
+++ src/common/pipes/xss-validation/xss-validation.pipe.ts
@@ -1,18 +1,9 @@
-import {
-  PipeTransform,
-  Injectable,
-  BadRequestException,
-  Logger,
-} from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import xss from 'xss';
 
 @Injectable()
 export class XssValidationPipe implements PipeTransform {
-  private readonly logger = new Logger(XssValidationPipe.name);
-
   transform(value: any): any {
-    this.logger.debug('Input value:', value);
-
     if (value && typeof value === 'string') {
       // Use the xss library to sanitize the input
       const sanitizedValue = xss(value);
